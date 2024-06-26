d6dd0e6918fa88fa15725e6b6627fe158abf0f83
tanguy <david.2908@apside-groupe.com>
2024-06-26 15:05:10 +0200

    en envoie les mails du client

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+var_dump($contactFormData);
+            $message = (new TemplatedEmail())
+                ->from('mailpourenvoi@lafamilledavid.fr')
+                ->to('davidtanguy@gmail.com')
+                ->subject('Demande client via le site adivec')
+                ->htmlTemplate('emails/demande_client.html.twig')
+                // pass variables (name => value) to the template
+                ->context([
+                    'formdata' => $contactFormData,
+                ]);
+            $mailer->send($message);
+            $this->addFlash('success', 'Vore message a été envoyé');
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
