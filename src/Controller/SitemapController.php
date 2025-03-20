a55301724b08c15311da098e38cbffffb1bb5366
tanguy <david.2908@apside-groupe.com>
2025-03-20 15:16:41 +0100

    sent to prod

---
+++ src/Controller/SitemapController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BlogPost;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SitemapController extends AbstractController
+{
+    #[Route('/sitemap.xml', name: 'sitemap', defaults: ['_format' => 'xml'])]
+    public function index(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        // Nous récupérons le nom d'hôte depuis l'URL
+        $hostname = $request->getSchemeAndHttpHost();
+
+        // On initialise un tableau pour lister les URLs
+        $urls = [];
+
+        // On ajoute les URLs "statiques"
+        $urls[] = ['loc' => $this->generateUrl('app_home')];
+        $urls[] = ['loc' => $this->generateUrl('app_about')];
+        $urls[] = ['loc' => $this->generateUrl('app_contact')];
+        $urls[] = ['loc' => $this->generateUrl('app_legal')];
+        $urls[] = ['loc' => $this->generateUrl('app_news')];
+        $urls[] = ['loc' => $this->generateUrl('app_familles')];
+
+        $articles = $entityManager->getRepository(BlogPost::class)->findByVisibles();
+        // On ajoute les URLs dynamiques des articles dans le tableau
+        foreach ($articles as $article) {
+            $images = [
+                'loc' => '/uploads/images/blog/' . $article->getImage(), // URL to image
+                'title' => $article->getTitle()    // Optional, text describing the image
+            ];
+
+            $urls[] = [
+                'loc' => $this->generateUrl('app_news', [
+                    'slug' => $article->getId(),
+                ]),
+                'image' => $images
+            ];
+        }
+        // Fabrication de la réponse XML
+        $response = new Response(
+            $this->renderView('sitemap/index.html.twig', [
+                'urls' => $urls,
+                'hostname' => $hostname
+            ]),
+            200
+        );
+
+        // Ajout des entêtes
+        $response->headers->set('Content-Type', 'text/xml');
+
+        // On envoie la réponse
+        return $response;
+    }
+}
